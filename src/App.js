01162614d6e6c78cfa190bc7547e9c08e87e7879
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-11 17:08:55 +0530

    Real Time Input Form Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import ShowUserInput from "./ShowUserInput";
 // import HelloWorld from "./HelloWorld";
-import CounterApp from "./CounterApp";
+// import CounterApp from "./CounterApp";
 
 const App = () => {
   return (
     <div>
       {/* <HelloWorld /> */}
-      <CounterApp />
+      {/* <CounterApp /> */}
+      <ShowUserInput />
     </div>
   );
 };
