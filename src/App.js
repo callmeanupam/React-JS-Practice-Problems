a66689327fbda9d8ef8b47f7dcd0f5bca58b306d
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-11 16:54:19 +0530

    Hello World Added in App.Js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import HelloWorld from "./HelloWorld";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <HelloWorld />
     </div>
   );
 };
